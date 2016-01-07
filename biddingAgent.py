d500163abbd0150b88b0e2a596b0c92c73581598
Sage Vouse <Sagevouse@ip-79-19.wireless.oberlin.edu>
2016-01-06 20:49:19 -0500

    Rough mock-up of abstract glass for bidding agents

---
+++ biddingAgent.py
@@ -0,0 +1,30 @@
+
+
+
+class biddingAgent:
+    print('Welcome to the space station auction contest! You are bidding against %s participants,'
+              ' and are starting with a budget of %f. The following cards will be auctioned:' %(players, budget))
+    print(cards)
+    def __init__(self, cards, identification, budget, players):
+        pass
+
+
+    def getName(self):
+        pass
+
+
+
+    def getColor(self):
+        pass
+
+
+
+    def getBid(self, card, index):
+        pass
+
+
+
+
+    def seeResults(card, winner, price, bids):
+        pass
+    
