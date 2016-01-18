61b30a710fed41ae7382f3b0e96c547fa178eb75
Nick Music <nmusic@clyde>
2016-01-18 00:40:48 -0500

    re-wrote/improved gui

--- auction_gui.py
+++ auction_gui.py
@@ -1,112 +1,120 @@
-# This is very incomplete, but I hoped to at least outline some kind framework
-# That we can build on. Have a look at critter_gui.py to see what I have been
-# refering to.
-
 from tkinter import *
+import NickCardGenerator
 import card
 
 class AuctionGUI():
+    
+    def __init__(self, width, height):
+        self.wide = width
+        self.high = height
+        self.generator = NickCardGenerator.NickCardGenerator(10)
+        self.deck = self.generator.buildDeck()
 
-    def __init__(self, model, gui_functions, defaults, scale, num_bidders):
-        self.model = model #is this the same as master?
-        self.gui_functions = gui_functions #"secure all the gui functions"
-        self.SCALE_FACTOR = scale #simpler way to deal with various screen sizes
-        self.num_bidders = num_bidders #number of players
-        self.is_running = False #keep track of whether the auction is running
-        self.budget = 100.00 #starting budget
-        self.initialize_graphics(1)# 1 is temporary...
-        self.root.mainloop() #keep window open unti auction ends or its closed
+    def initialize_graphics(self):
 
-    def initialize_graphics(self, defaults):
-        # GUI Window
+        # ROOT WINDOW
         self.root = Tk()
-        self.root.title("Space Station Auction")
-        self.root.minsize(width=800, height=600) #get from defaults perhaps?
+        self.root.title("Space Station Auction!")
 
-        # Left Panel (card list)
+        # *LEFT PANEL*
         self.left = Frame(self.root)
-        self.left.pack(side=LEFT, fill=Y)
+        self.left.pack(side=LEFT, fill=BOTH, expand=1)
 
-        self.left_header = Message(self.left, text="MODULES", bd=5, bg="#666699",
-                                  relief=RAISED, width=100, fg="white")
-        self.left_header.pack(side=TOP, fill=X)
+        # bid result history
+        self.bid_history = Frame(self.left)
+        self.bid_history.pack(side=TOP, fill=X)
 
-        self.cards = Message(self.left, text="temporary", bd=5, bg="#ccccff", anchor=NW,
-                        relief=RIDGE)
-        self.cards.pack(side=TOP, fill=BOTH, expand=1)
+        self.S = Scrollbar(self.bid_history)
+        self.T = Text(self.bid_history, height=4, bg="black", fg="green",
+                      state=DISABLED)
+        self.S.pack(side=RIGHT, fill=Y)
+        self.T.pack(side=LEFT, fill=X, expand=1)
+        self.S.config(command=self.T.yview)
+        self.T.config(yscrollcommand=self.S.set)
 
-        # Middle Panel (card/graph display and buttons)...maybe buttons seperate?
-        self.middle = Frame(self.root)
-        self.middle.pack(side=LEFT, fill=BOTH, expand=1)
+        self.T.config(state=NORMAL)
+        self.T.insert(END, "player5 bought 'Puppy Cloning Center' for $65")
+        self.T.config(state=DISABLED)
 
-        self.canvas = Canvas(self.middle)
-        self.background = PhotoImage(file='background.gif')
-        self.canvas.create_image(0,0, anchor=NW, image=self.background)
-        self.canvas.pack(fill=BOTH, expand=1)
+        # canvas for player info, graph, etc
+        self.graph = Canvas(self.left, bg="black")
 
-        # Buttons
-        self.play_pause_button = Button(self.middle, text="START", bg="green")
-        self.next_button = Button(self.middle, text="NEXT", bg="yellow")
-        self.quit_button = Button(self.middle, text="RESET", bg="red")
-        self.info_button = Button(self.middle, text="?")
+        self.img = PhotoImage(file="background.gif")
+        self.graph.create_image(0,0, anchor=NW, image=self.img)
 
-        self.play_pause_button.pack(side=LEFT, fill=X, expand=1)
-        self.next_button.pack(side=LEFT, fill=X, expand=1)
-        self.quit_button.pack(side=LEFT, fill=X, expand=1)
-        self.info_button.pack(side=LEFT, fill=X, expand=1)
+        self.graph.pack(side=TOP, fill=BOTH, expand=1)
 
-        # Right Panel (player list)
+        # buttons
+        self.buttons = Frame(self.left, bg="grey", bd=5)
+        self.buttons.pack(side=BOTTOM, fill=X)
+
+        self.play_pause_button = Button(self.buttons, bg="#33ff33", text="PLAY",
+                                        width=10, activebackground="green")
+        self.step_button = Button(self.buttons, bg="#ffff33", text="STEP",
+                                  width=10, command=self.step, activebackground="yellow")
+        self.quit_button = Button(self.buttons, bg="#ff3333", text="QUIT",
+                                  width=10, command=self.root.destroy, activebackground="red")
+        self.info_button = Button(self.buttons, bg="grey", text="?",
+                                  width=5, activebackground="#ff3333")
+
+        self.play_pause_button.pack(side=LEFT)
+        self.step_button.pack(side=LEFT, padx=20)
+        self.quit_button.pack(side=LEFT)
+        self.info_button.pack(side=RIGHT)
+
+        # *RIGHT PANEL*
         self.right = Frame(self.root)
         self.right.pack(side=RIGHT, fill=Y)
 
-        self.right_header = Message(self.right, text="BIDDERS", bd=5, bg="#666699", anchor=NW,
-                                   relief=RAISED, width=100, fg="white")
-        self.right_header.pack(side=TOP, fill=X)
+        # current card
+        self.current = Canvas(self.right, width =(self.wide+10),
+                              height=(self.high+10), bg="black")
+        self.current.pack(side=TOP)
 
-        self.players = Message(self.right, text="temporary", bd=5, bg="#ccccff", anchor=NW,
-                               relief=RIDGE)
-        self.players.pack(side=TOP, fill=BOTH, expand=1)
-                                   
-    """
-    def make_buttons(self):
+        #card queue
+        self.queue = Canvas(self.right, width=(self.wide+10), bg="black")
+        self.queue.pack(side=TOP, fill=Y, expand=1)
 
-    def bind_keys(self):
-
-        # Key Bindings for different actions (when we have them)
-
-        self.root.bind("<Escape>", lambda *args: exit())
-
-    def play_pause(self):
-
-        #stop or start the auction
-
-    def next_card(self):
-    """
-    # not working. there is a special thing we need to do with the gui functions becuase once
-    # it is initialized, it just stays in its own loop. There is a method to correct this
-    # in the critter lab that I am trying to figure out. Feel free to try and implement
-    # this if you see how it works.
-    def draw_card(self, card):
+    def step(self):
+        self.draw_card()
+        self.update_queue()
+        self.update_info()
         
-        self.canvas.create_rectangle(15,15,320,420, fill="#00e5e6", outline="#19334d", width=5)
-        self.canvas.create_rectangle(35,35,300,400, fill="#19334d")
-        self.canvas.create_text(168,50, anchor=N, fill="#00e5e6", width=230,
-                            text=card.getName(), font=("Helvetica", "26"),
-                            justify=CENTER)
+    def draw_card(self):
+        if self.deck[0]:
+            card = self.deck[0]
+            """
+            self.current.create_arc(5,5,17,17, start=90, extent=90, outline="white")
+            self.current.create_arc(173,5,185,17, start=0, extent=90, style=ARC, outline="white")
+            self.current.create_arc(173,178,185,195, start=270, extent=90, style=ARC, outline="white")
+            self.current.create_arc(5,178,17,195, start=180, extent=90, style=ARC, outline="white")
+            """
+            self.current.create_rectangle(5,5,190,200, fill="#19334d", outline="#00e5e6", width=10)
+            #self.current.create_rectangle(35,35,300,400, fill="#19334d")
+            self.current.create_text(95,20, anchor=N, fill="#00e5e6", width =150,
+                        text=card.getName(), font=("Helvetica", "16"),
+                        justify=CENTER)
+            self.current.create_text(95,150, anchor=N, fill="#00e5e6", width =150,
+                        text="[" + str(card.getStats()[0]) +
+                   ", " + str(card.getStats()[1]) +
+                   ", " + str(card.getStats()[2]) +
+                   ", " + str(card.getStats()[3]) +
+                   ", " + str(card.getStats()[4]) + "]", 
+                        font=("Helvetica", "16"),
+                        justify=CENTER)
+            self.deck.remove(self.deck[0])
 
-    """
-    def draw_graph(self):
+    def update_queue(self):
+        pass
 
-    def update_graph(self):
+    def update_info(self):
+        pass
+        
 
-    def update_bidders(self):
-
-    def update_cards(self):
-    """
-
-def main(): #for testing purposes
-    gui = AuctionGUI(1,1,1,1,1)
-    card1 = card.Card("Radiation Treatment Center", [1,5,0,0,0])
-    gui.draw_card(card1) #testing draw_card
+def main():
+    gui = AuctionGUI(180, 190)
+    gui.initialize_graphics()
+    #gui.make_buttons()
+    gui.root.mainloop()
 
 main()
