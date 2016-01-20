3936a3902575542f449b65e7eb69412dae1c8d2e
nickmusic <nmusic@oberlin.edu>
2016-01-19 23:53:15 -0500

    added legend when mouse is over '?' button, appearance is temporary

--- auction_gui.py
+++ auction_gui.py
@@ -1,13 +1,13 @@
 from tkinter import *
-import card_generator
+import FinishedCardGenerator
 import card
 
 class AuctionGUI():
     
     def __init__(self, width, height):
-        self.wide = width
-        self.high = height
-        self.deck = card_generator.buildDeck(40)
+        self.width = width
+        self.height = height
+        self.deck = FinishedCardGenerator.buildDeck(40)
 
     def initialize_graphics(self):
 
@@ -55,24 +55,28 @@ class AuctionGUI():
         self.quit_button = Button(self.buttons, bg="#ff3333", text="QUIT",
                                   width=10, command=self.root.destroy, activebackground="red")
         self.info_button = Button(self.buttons, bg="grey", text="?",
-                                  width=5, activebackground="#ff3333")
+                                  width=5, relief=GROOVE)
 
         self.play_pause_button.pack(side=LEFT)
         self.step_button.pack(side=LEFT, padx=20)
         self.quit_button.pack(side=LEFT)
         self.info_button.pack(side=RIGHT)
 
+        # test with mouse over legend
+        self.info_button.bind('<Enter>', self.legend)
+        self.info_button.bind('<Leave>', self.remove_legend)
+
         # *RIGHT PANEL*
         self.right = Frame(self.root)
         self.right.pack(side=RIGHT, fill=Y)
 
         # current card
-        self.current = Canvas(self.right, width =(self.wide+10),
-                              height=(self.high+10), bg="black")
+        self.current = Canvas(self.right, width =(self.width+10),
+                              height=(self.height+10), bg="black")
         self.current.pack(side=TOP)
 
         #card queue
-        self.queue = Canvas(self.right, width=(self.wide+10), bg="black")
+        self.queue = Canvas(self.right, width=(self.width+10), bg="black")
         self.queue.pack(side=TOP, fill=Y, expand=1)
 
     def step(self):
@@ -107,6 +111,9 @@ class AuctionGUI():
                         justify=CENTER)
             self.deck.remove(self.deck[0])
 
+    def draw_cardlet():
+        pass
+
     def add_history(self, message):
         self.T.config(state=NORMAL)
         self.T.insert(END, "\n" + message)
@@ -118,8 +125,32 @@ class AuctionGUI():
 
     def update_info(self):
         pass
-        
 
+    def legend(self, event):
+        width = self.graph.winfo_width()
+        height = self.graph.winfo_height()
+        x1 = width-self.width-5
+        y1 = height-self.height-5
+        x2 = width-5
+        y2 = height-5
+        self.a = self.graph.create_rectangle(x1,y1,x2,y2, fill="grey", width=5)
+        self.b = self.graph.create_text((x2-100), y1+(self.height*(1/6)), text="Science", anchor=W, font=("Helvetica", "12", "bold"))
+        self.c = self.graph.create_text((x2-100), y1+(self.height*(2/6)), text="Ecology", anchor=W, font=("Helvetica", "12", "bold"))
+        self.d = self.graph.create_text((x2-100), y1+(self.height*(3/6)), text="Culture", anchor=W, font=("Helvetica", "12", "bold"))
+        self.e =self.graph.create_text((x2-100), y1+(self.height*(4/6)), text="Commerce", anchor=W, font=("Helvetica", "12", "bold"))
+        self.f = self.graph.create_text((x2-100), y1+(self.height*(5/6)), text="Industry", anchor=W, font=("Helvetica", "12", "bold"))
+
+        self.g = self.graph.create_oval((x2-150), y1+(self.height*(1/6))-10, (x2-130), y1+(self.height*(1/6))+10, fill="blue")
+        self.h = self.graph.create_oval((x2-150), y1+(self.height*(2/6))-10, (x2-130), y1+(self.height*(2/6))+10, fill="green")
+        self.i = self.graph.create_oval((x2-150), y1+(self.height*(3/6))-10, (x2-130), y1+(self.height*(3/6))+10, fill="purple")
+        self.j = self.graph.create_oval((x2-150), y1+(self.height*(4/6))-10, (x2-130), y1+(self.height*(4/6))+10, fill="yellow")
+        self.k = self.graph.create_oval((x2-150), y1+(self.height*(5/6))-10, (x2-130), y1+(self.height*(5/6))+10, fill="red")
+
+    def remove_legend(self, event):
+        self.graph.delete(self.a,self.b,self.c,self.d,self.e,
+                          self.f,self.g,self.h,self.i,self.j,
+                          self.k)
+        
 def main():
     gui = AuctionGUI(180, 190)
     gui.initialize_graphics()
