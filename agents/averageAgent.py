c984db1530a04b381739ea3325a2737e9f321f2e
Aaron Young <aarony422@gmail.com>
2016-01-28 00:08:39 +0800

    added average and half in agent

--- agents/averageAgent.py
+++ agents/averageAgent.py
@@ -1,22 +1,30 @@
 import biddingAgent
 
-class AllInAgent(biddingAgent.biddingAgent):
+class AverageAgent(biddingAgent.biddingAgent):
     def __init__(self, cards, ID, players, budget):
         self.ID=ID
         self.budget = budget
+        self.numRounds = 0
+        self.totalBid = 0
 
     def getName(self):
-        return "All In"
+        return "Average"
 
     def getColor(self):
         return "#24ee93"
 
+# Bids the average of all previous winning bids
     def getBid(self, card, index):
-        return self.budget
+        if (self.numRounds == 0):
+            return 0
+        currBid = self.totalBid//self.numRounds
+        return min(currBid, self.budget)
 
     def seeResults(self, card, winner, price, bids):
         if winner == self.ID:
             self.budget -= price
+        self.totalBid += price
+        self.numRounds += 1
 
     def getBudget(self):
         return self.budget
--- agents/halfInAgent.py
+++ agents/halfInAgent.py
@@ -1,18 +1,18 @@
 import biddingAgent
 
-class AllInAgent(biddingAgent.biddingAgent):
+class HalfInAgent(biddingAgent.biddingAgent):
     def __init__(self, cards, ID, players, budget):
         self.ID=ID
         self.budget = budget
 
     def getName(self):
-        return "All In"
+        return "Half In"
 
     def getColor(self):
-        return "#24ee93"
+        return "#eceb7b"
 
     def getBid(self, card, index):
-        return self.budget
+        return (self.budget)//2
 
     def seeResults(self, card, winner, price, bids):
         if winner == self.ID:
